05be9dbfa7b1a65b9e22dc22f17ca0bb4a73feb6
Your NameLiaaaaaaaaaaagit config --global user.email you@example.comyllia_vladimirovna@mail.ru <you@example.com>
2022-08-12 20:49:40 +0300

    feat: add change the background

---
+++ momentum/index.js
@@ -0,0 +1,167 @@
+// Смена фонового изображения +20
+const imagesMorning = [
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/01.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/02.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/03.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/04.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/05.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/06.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/07.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/08.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/09.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/10.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/11.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/12.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/13.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/14.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/15.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/16.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/17.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/18.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/19.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/morning/20.jpg'
+];
+const imagesAfternoon = [
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/01.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/02.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/03.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/04.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/05.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/06.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/07.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/08.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/09.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/10.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/11.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/12.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/13.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/14.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/15.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/16.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/17.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/18.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/19.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/afternoon/20.jpg'
+];
+const imagesEvening = [
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/01.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/02.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/03.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/04.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/05.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/06.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/07.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/08.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/09.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/10.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/11.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/12.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/13.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/14.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/15.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/16.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/17.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/18.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/19.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/evening/20.jpg'
+];
+const imagesNight = [
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/01.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/02.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/03.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/04.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/05.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/06.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/07.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/08.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/09.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/10.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/11.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/12.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/13.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/14.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/15.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/16.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/17.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/18.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/19.jpg',
+    'https://raw.githubusercontent.com/Liaaaaaaaaaaa/stage1-tasks/assets/images/night/20.jpg'
+];
+
+let date = new Date();
+let time = date.getHours();
+
+function timeOfDay() {
+    if (time >= 0 && time < 6) return imagesNight;
+    if (time >= 6 && time < 12) return imagesMorning;
+    if (time >= 12 && time < 18) return imagesAfternoon;
+    if (time >= 18 ) return imagesEvening
+};
+
+// console.log(time);
+
+// setInterval("viewImages()", 1000);
+
+// console.log(timeOfDay());
+
+function mixImage(arr) {
+    return arr.map(i => [Math.random(), i]).sort().map(i => i[1])
+};
+
+
+let imgUrl = mixImage(timeOfDay());
+
+document.body.style.background = 'url("' + imgUrl[1] + '")';
+
+function imgNumber() {
+    if (timeOfDay() === imagesAfternoon) {
+        return imgUrl[1].slice(85, 87)
+    }
+    if (timeOfDay() === imagesNight) {
+        return imgUrl[1].slice(81, 82)
+    } else {
+        return imgUrl[1].slice(83, 85)
+    }
+}
+
+let timeOfDayNow = timeOfDay();
+
+let imgNumberNow = imgNumber();
+
+// console.log (imgNumberNow);
+// console.log (timeOfDayNow);
+// console.log(timeOfDayNow[imgNumberNow]);
+
+
+const slidePrev = document.querySelector('.slide-prev');
+const slideNext = document.querySelector('.slide-next');
+
+function SwitchPrev () {
+    if (imgNumberNow === 1 ) {
+        document.body.style.background = 'url("' + timeOfDayNow[19]+ '")';
+        imgNumberNow = 20;
+    }
+     else { 
+        document.body.style.background = 'url("' + timeOfDayNow[imgNumberNow -2] + '")';
+        imgNumberNow = imgNumberNow-1
+    }
+} ;
+
+
+     slidePrev.onclick = SwitchPrev;
+
+ 
+function SwitchNext () {
+    if (imgNumberNow === 20 ) {
+        document.body.style.background = 'url("' + timeOfDayNow[0]+ '")';
+        imgNumberNow = 1;
+    }
+     else { 
+        document.body.style.background = 'url("' + timeOfDayNow[imgNumberNow ] + '")';
+        imgNumberNow++
+    }
+} ;
+
+
+     slideNext.onclick = SwitchNext;
+
